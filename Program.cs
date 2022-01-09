9e258e124b40443d00df086f99c838f9ac1e8c95
Aleksander Filonenko <Education-4-IT@ya.ru>
2022-01-09 18:24:32 +0300

    add functions

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿// Задача: Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел.
 // Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. При решении не
 // рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+int [] FstArray(int length, int min, int max)
+{int [] array = new int [length];
+for (int i = 0; i< length; i++)
+ {array[i] = new Random().Next(min, max + 1);}
+ return array;}
+ 
+ void PrintArray (int [] arr)
+ {
+     for (int i = 0; i < arr.Length; i++)
+     {System.Console.Write($"{arr[i]} ");}}
+ 
+ int [] CheckEvenElements (int[] a)
+ { int[] b = new int [a.Length];
+    int j = 0;
+    for (int i = 0; i< a.Length; i++)
+    { if (a[i] % 2 == 0)
+    {b[j]= a[i];j++;}}
+Array.Resize(ref b,j);
+return b;}
